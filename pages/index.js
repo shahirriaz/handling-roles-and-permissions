ea0a8eaf116bd25a0ad5ecfd6d646ad782cc9d8a
prince7866 <71831042+prince7866@users.noreply.github.com>
2022-06-25 21:38:48 +0200

    m

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="columns-3" >
+      <div>
+        <h1>Featured news</h1>
+          {/*{Array.from(({length: 6})).map(() =)} */}
+      </div>
+      <div>
+        <h1>Latest news</h1>
+      </div>
+      <div>
+        <h1>Most read</h1>
+      </div>
     </div>
   );
 }
